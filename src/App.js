6a741ea73562346cebbaef065227029f78bd1afb
Shubham Anchliya <info.shubhamanchliya@gmail.com>
2022-08-24 19:23:12 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import { useDispatch, useSelector } from "react-redux";
+import { decrement, increment } from "./reducers/counterSlice";
 
-function App() {
+const App = () => {
+  const count = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <div className="App container mt-5 text-center">
+
+        <h2 className="mb-3">React Js Redux Store with Slice State Example</h2>
+        
+        <p className="h1">{count}</p>
+
+        <button
+          onClick={() => {
+            dispatch(increment());
+          }}
+          className="me-2 btn btn-primary"
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          Increment
+        </button>
+
+        <button
+          onClick={() => {
+            dispatch(decrement());
+          }}
+          className="me-2 btn btn-danger"
+        >
+          Decrement
+        </button>
+
+      </div>
+    </>
   );
-}
+};
 
 export default App;
